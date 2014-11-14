0b1ad1b0498e82ff1cfcaa5c0061e066c2880302
James Moey <james.moey@news.com.au>
2014-11-14 16:41:24 +1100

    Add missing shebang

--- start-etcd-service-registry.sh
+++ start-etcd-service-registry.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/export LOCAL_PORT=\1\nexport REMOTE_HOST=\2\nexport REMOTE_PORT=\3/' > /tmp/env
 
 echo "export LOCAL_HOST=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`" >> /tmp/env
